a642c47e5d99e94ec8db3cb2eb4cad124eb7c6ef
Christopher Wright <cmwright33@gmail.com>
2013-06-19 10:45:43 -0400

    added the instructer led calculator creation

---
+++ pbcalc.rb
@@ -0,0 +1,47 @@
+def add(a, b)
+  return a + b
+end
+
+def subtract(a, b)
+  return a - b
+end
+
+def multiply(a, b)
+  return a * b
+end
+
+def divide(a, b)
+  return a / b
+end
+
+def prompt_numbers
+  puts "Give me a number"
+  a = gets.chomp.to_f
+  puts "Give me another number"
+  b = gets.chomp.to_f
+  return a, b
+end
+
+def prompt_function
+  puts 'Add, subtract, multiply, divide?'
+  return gets.chomp
+end
+
+def prompt
+  function = prompt_function
+  a, b = prompt_numbers
+  return a, b, function
+end
+
+a, b, math_function = prompt
+
+puts case math_function
+when "add"
+  add(a, b)
+when "subtract"
+  subtract(a, b)
+when "multiply"
+  multiply(a,b)
+when "divide"
+  divide(a,b)
+end
